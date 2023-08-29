a8f455f703e954fb8d2bdd4978fffc4c7005fefa
takedatakashiki <takedatakashiki@takedatakashikinoMacBook-Pro.local>
2023-08-29 14:28:01 +0900

    start project lol

---
+++ AutomateMemoryLeakDetection/AutomateMemoryLeakDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutomateMemoryLeakDetection
+//
+//  Created by takedatakashiki on 2023/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
